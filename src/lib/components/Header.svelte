746cf54c33d00387aa65d1a76b115133cdfb5204
Domenik Reitzner <domenik@reitzner.at>
2022-05-12 21:50:43 +0200

    Add some case styles

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,10 @@
 <header>
+    <img src="/logos/Logo-Dark-Tagline.svg" alt="Logo Svelte Society Vienna">
     <nav><a href="/">home</a><a href="/svienna">svienna - svelte society vienna</a></nav>
 </header>
+
+<style>
+    header {
+        background: var(--black-light);
+    }
+</style>

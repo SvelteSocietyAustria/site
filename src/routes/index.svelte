6387652849a4a6c3092a8f7dcf292e5dd646d90d
Domenik Reitzner <domenik@reitzner.at>
2022-05-16 13:31:03 +0200

    ✨feature: add Sponsor section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-import GitHubAvatar from "../lib/components/GitHubAvatar.svelte";
-
+	import GitHubAvatar from '../lib/components/GitHubAvatar.svelte';
 
 	const founders: { displayName: string; twitter: string; githubAuthor: string }[] = [
 		{
@@ -40,18 +39,31 @@ import GitHubAvatar from "../lib/components/GitHubAvatar.svelte";
 	{#each founders as { displayName, twitter, githubAuthor }}
 		<li>
 			<div class="founder">
-				<GitHubAvatar {githubAuthor} size={70}></GitHubAvatar>
+				<GitHubAvatar {githubAuthor} size={70} />
 				<div class="ml-4">
 					<h3>{displayName}</h3>
 					<p>
 						<a href={twitter} target="_blanck" rel="noopener noreferrer">twitter</a> /
-						<a href="https://github.com/{githubAuthor}" target="_blanck" rel="noopener noreferrer">github</a>
+						<a href="https://github.com/{githubAuthor}" target="_blanck" rel="noopener noreferrer"
+							>github</a
+						>
 					</p>
 				</div>
 			</div>
 		</li>
 	{/each}
 </ul>
+
+<section class="sponsors">
+	<h2>Sponsors</h2>
+	<div class="flex -list">
+		<a class="flex" href="https://www.emakina.com/">
+			<img src="/logos/Logo-Emakina.svg" alt="Emakina CEE Logo" />
+			Emakina CEE
+		</a>
+		<a class="flex" href="mailto:reitznerdomenik+svelteaustria@gmail.com"> Contact us </a>
+	</div>
+</section>
 
 <style>
 	h2 {
@@ -73,5 +85,25 @@ import GitHubAvatar from "../lib/components/GitHubAvatar.svelte";
 
 	.ml-4 {
 		margin-left: 1rem;
+	}
+
+	.sponsors {
+		margin-top: 8rem;
+	}
+	.sponsors img {
+		max-width: 120px;
+	}
+	.sponsors a {
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		border: 3px solid var(--color-black-light);
+		padding: 1rem;
+		margin-right: 1rem;
+		border-radius: 0.5rem;
+		gap: 0.5rem;
+	}
+	.sponsors .-list {
+		margin: 2rem 0;
 	}
 </style>

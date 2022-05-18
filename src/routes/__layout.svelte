d963dd53505ed90fd98dc1daee15f54377246796
Domenik Reitzner <domenik@reitzner.at>
2022-05-18 17:31:30 +0200

    Add some seo stuff 🔍

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import { afterNavigate } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
+	let title: string;
+
+	afterNavigate(({ to }) => {
+		const firstUrlSegment = to.pathname.split('/')[1] || 'Home';
+		title = firstUrlSegment.slice(0, 1).toUpperCase() + firstUrlSegment.slice(1);
+	});
 </script>
+
+<svelte:head>
+	<meta property="og:title" content="Svelte Society Austria - {title}" />
+	<meta property="og:url" content={$page.url.href} />
+</svelte:head>
 
 <Header />
 <main><slot /></main>

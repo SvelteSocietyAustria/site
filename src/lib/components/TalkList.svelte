c479d9f13dc04265296efbafe73e4d0d3a74cba2
Domenik Reitzner <domenik@reitzner.at>
2022-07-05 11:16:40 +0200

    Update past talk logic 🚂

---
+++ src/lib/components/TalkList.svelte
@@ -0,0 +1,145 @@
+<script lang="ts">
+	import type { TMeetup } from '$lib/components/TSvienna';
+	import GitHubAvatar from '$lib/components/GitHubAvatar.svelte';
+	import By from '$lib/components/By.svelte';
+
+	export let meetups: TMeetup[];
+
+	function getMonth(dateISO: string) {
+		const months = [
+			'Jan.',
+			'Feb.',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'Aug.',
+			'Sept.',
+			'Oct.',
+			'Nov.',
+			'Dec.'
+		];
+		const d = new Date(dateISO);
+		return months[d.getMonth()];
+	}
+</script>
+
+{#if meetups.length}
+	<ul>
+		{#each meetups as { dateISO, name, lumaLink, by, talks }}
+			<li data-day={new Date(dateISO).getDate()} data-month={getMonth(dateISO)}>
+				<section>
+					<p class="meetupInfo">
+						<a href={lumaLink} target="_blank" rel="noopener noreferrer">{name}</a>
+						<By {by} />
+					</p>
+				</section>
+			</li>
+
+			{#each talks as { githubAuthor, name }}
+				<div class="talk">
+					<img
+						width="20"
+						height="20"
+						src="/logos/Logo-Vienna.svg"
+						alt="Svelte Society Austria Logo"
+					/>
+					<span>
+						{name}
+					</span>
+					<span><i>by</i></span>
+					{#if githubAuthor}
+						<GitHubAvatar {githubAuthor} />
+					{:else}
+						You?
+					{/if}
+					<!-- Add video here -->
+				</div>
+			{:else}
+				<div class="talk">
+					<i> No talks to announce yet. </i>
+				</div>
+			{/each}
+		{/each}
+	</ul>
+{:else}
+	<p>
+		Come back soon for more talks and follow us on <a
+			href="https://twitter.com/svelteaustria"
+			target="_blank"
+			rel="noopener noreferrer">twitter</a
+		>
+		and <a href="https://lu.ma/svienna" target="_blank" rel="noopener noreferrer">Luma</a> to keep up
+		to date.
+	</p>
+{/if}
+
+<style>
+	.talk {
+		margin-top: 1rem;
+		display: flex;
+		align-items: center;
+	}
+	.talk > * {
+		margin-right: 1rem;
+	}
+
+	ul {
+		margin-top: 2rem;
+		padding-left: 3.5rem;
+		list-style: none;
+		--border-radius: 0.5rem;
+	}
+	li {
+		margin-top: 2rem;
+		position: relative;
+		height: 3.5rem;
+		display: flex;
+		align-items: center;
+	}
+
+	li::before,
+	li::after {
+		position: absolute;
+		left: -3rem;
+		width: 2.8rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		z-index: 1;
+	}
+	li::before {
+		content: attr(data-day);
+		top: 0;
+		background-color: var(--color-white);
+		color: var(--color-black);
+		height: 2rem;
+		font-size: 1.2rem;
+		border-top-left-radius: var(--border-radius);
+		border-top-right-radius: var(--border-radius);
+	}
+	li::after {
+		content: attr(data-month);
+		bottom: 0;
+		background-color: var(--color-red);
+		height: 1.5rem;
+		font-size: 0.7rem;
+		border-bottom-left-radius: var(--border-radius);
+		border-bottom-right-radius: var(--border-radius);
+	}
+
+	section {
+		height: calc(100% - 6px);
+		padding-right: 0.8rem;
+		padding-left: 1.6rem;
+		display: flex;
+		align-items: center;
+		transform: translateX(-0.8rem);
+		background-color: var(--color-black-light);
+		border: 3px solid var(--color-white);
+		border-left: none;
+		border-top-right-radius: var(--border-radius);
+		border-bottom-right-radius: var(--border-radius);
+	}
+</style>
--- src/routes/meetups/index.svelte
+++ src/routes/meetups/index.svelte
@@ -1,140 +1,29 @@
 <script lang="ts">
-	import By from '$lib/components/By.svelte';
 	import type { TMeetup } from '$lib/components/TSvienna';
-	import GitHubAvatar from '$lib/components/GitHubAvatar.svelte';
+	import TalkList from '$lib/components/TalkList.svelte';
 
 	export let meetups: TMeetup[];
 
-	function getMonth(dateISO: string) {
-		const months = [
-			'Jan.',
-			'Feb.',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'Aug.',
-			'Sept.',
-			'Oct.',
-			'Nov.',
-			'Dec.'
-		];
-		const d = new Date(dateISO);
-		return months[d.getMonth()];
-	}
+	const today = new Date();
+
+	let pastMeetups: TMeetup[] = [];
+	let futureMeetups: TMeetup[] = [];
+
+	meetups.forEach((m) => {
+		new Date(m.dateISO) < today
+			? (pastMeetups = [...pastMeetups, m])
+			: (futureMeetups = [...futureMeetups, m]);
+	});
 </script>
 
 <h1>Planned Meetups</h1>
-<ul>
-	{#each meetups as { dateISO, name, lumaLink, by, talks }}
-		<li data-day={new Date(dateISO).getDate()} data-month={getMonth(dateISO)}>
-			<section>
-				<p class="meetupInfo">
-					<a href={lumaLink} target="_blank" rel="noopener noreferrer">{name}</a>
-					<By {by} />
-				</p>
-			</section>
-		</li>
-
-		{#each talks as { githubAuthor, name }}
-			<div class="talk">
-				<img
-					width="20"
-					height="20"
-					src="/logos/Logo-Vienna.svg"
-					alt="Svelte Society Austria Logo"
-				/>
-				<span>
-					{name}
-				</span>
-				<span><i>by</i></span>
-				{#if githubAuthor}
-					<GitHubAvatar {githubAuthor} />
-				{:else}
-					You?
-				{/if}
-			</div>
-		{:else}
-			<div class="talk">
-				<i> No talks to announce yet. </i>
-			</div>
-		{/each}
-	{/each}
-</ul>
+<TalkList meetups={futureMeetups} />
 
 <h2>Passed Meetups</h2>
-<p>Nothing to see here... 👋</p>
+<TalkList meetups={pastMeetups} />
 
 <style>
-	.talk {
-		margin-top: 1rem;
-		display: flex;
-		align-items: center;
-	}
-	.talk > * {
-		margin-right: 1rem;
-	}
-
 	h2 {
 		margin-top: 2rem;
-	}
-
-	ul {
-		margin-top: 2rem;
-		padding-left: 3.5rem;
-		list-style: none;
-		--border-radius: 0.5rem;
-	}
-	li {
-		margin-top: 2rem;
-		position: relative;
-		height: 3.5rem;
-		display: flex;
-		align-items: center;
-	}
-
-	li::before,
-	li::after {
-		position: absolute;
-		left: -3rem;
-		width: 2.8rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		z-index: 1;
-	}
-	li::before {
-		content: attr(data-day);
-		top: 0;
-		background-color: var(--color-white);
-		color: var(--color-black);
-		height: 2rem;
-		font-size: 1.2rem;
-		border-top-left-radius: var(--border-radius);
-		border-top-right-radius: var(--border-radius);
-	}
-	li::after {
-		content: attr(data-month);
-		bottom: 0;
-		background-color: var(--color-red);
-		height: 1.5rem;
-		font-size: 0.7rem;
-		border-bottom-left-radius: var(--border-radius);
-		border-bottom-right-radius: var(--border-radius);
-	}
-
-	section {
-		height: calc(100% - 6px);
-		padding-right: 0.8rem;
-		padding-left: 1.6rem;
-		display: flex;
-		align-items: center;
-		transform: translateX(-0.8rem);
-		background-color: var(--color-black-light);
-		border: 3px solid var(--color-white);
-		border-left: none;
-		border-top-right-radius: var(--border-radius);
-		border-bottom-right-radius: var(--border-radius);
 	}
 </style>

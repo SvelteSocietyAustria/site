045a26775f1b88b5dc1c241033c297e198f35afa
Ivan Hofer <ivan.hofer@outlook.com>
2022-09-02 20:50:24 +0200

    open sponsors in new tab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,11 +32,11 @@
 <section class="sponsors">
 	<h2>Sponsors</h2>
 	<div class="flex -list">
-		<a class="flex" href="https://www.emakina.com/de-at">
+		<a class="flex" href="https://www.emakina.com/de-at" rel="noopener" target="_blank">
 			<img src="/logos/Logo-Emakina.svg" alt="Emakina CEE Logo" />
 			Emakina CEE
 		</a>
-		<a class="flex" href="https://www.iteratec.com/">
+		<a class="flex" href="https://www.iteratec.com/" rel="noopener" target="_blank">
 			<img src="/logos/Logo-iteratec.png" alt="iteratec GmbH Logo" />
 			iteratec GmbH
 		</a>

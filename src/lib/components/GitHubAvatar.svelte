29aac7ecf758ac917399adbd1172b996a105ef2e
jycouet <jycouet@gmail.com>
2022-10-28 20:47:08 +0200

    :bug: FIX: Update pic back to work & query string update

--- src/lib/components/GitHubAvatar.svelte
+++ src/lib/components/GitHubAvatar.svelte
@@ -1,27 +1,49 @@
 <script lang="ts">
+	import type { Action } from 'svelte/types/runtime/action';
+
 	export let githubAuthor: string;
 	export let size: number = 40;
 	export let asObjectUrl = false;
 
-	const url = `https://avatars.githubusercontent.com/${githubAuthor}?v=${new Date().getTime()}`;
-	let src = '';
+	$: url = `https://avatars.githubusercontent.com/${githubAuthor}?v=${new Date().getTime()}`;
+
 	const getObjectUrl = async () => {
 		try {
-			const res = await fetch(url, {
-				mode: 'cors',
-				redirect: 'follow',
-			});
-			const blob = await res.blob()
-			return URL.createObjectURL(blob);
+			if (githubAuthor) {
+				const res = await fetch(url, {
+					mode: 'cors',
+					redirect: 'follow'
+				});
+				const blob = await res.blob();
+				return URL.createObjectURL(blob);
+			}
 		} catch (error) {
 			console.log(error);
-			return url;
 		}
-	}
+		return '/github/unknown.png';
+	};
 
-	(async() => {
-		src = asObjectUrl ? await getObjectUrl() : url
-	})()
+	const actionImgSrc: Action<HTMLElement, any> = (node: any) => {
+		// initial img
+		refreshImg(node);
+		return {
+			// On update, refreshImg
+			update() {
+				refreshImg(node);
+			},
+			destroy() {}
+		};
+	};
+
+	const refreshImg = (node: any) => {
+		if (asObjectUrl) {
+			(async () => {
+				node.src = await getObjectUrl();
+			})();
+		} else {
+			node.src = url;
+		}
+	};
 </script>
 
 <a href="https://github.com/{githubAuthor}" target="_blanck" rel="noopener noreferrer">
@@ -29,7 +51,7 @@
 		class="rouned"
 		width={size}
 		height={size}
-		{src}
+		use:actionImgSrc={githubAuthor}
 		alt={`${githubAuthor} profile picture`}
 	/>
 </a>

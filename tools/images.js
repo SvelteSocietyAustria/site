68b6d8ce18261012d490ffd5e92fbd6da7bc6082
Domenik Reitzner <domenik@reitzner.at>
2022-11-03 09:12:55 +0100

    Add all transfomations 🎡

--- tools/images.js
+++ tools/images.js
@@ -1,13 +1,41 @@
-import { glob } from "glob-promise";
+import { promise as glob } from "glob-promise";
 import sharp from "sharp";
+import fs from 'fs/promises'
 
+////
+// helpers
+
+const SIZES = [
+    150,
+    640,
+    1024,
+    1280,
+    1920,
+];
+
+const getOutputPath = (originalPath) => {
+    const path = originalPath.split('gallery/')[1];
+    return `./src/lib/generated/${path}`;
+}
+
+const getImageDerivitePath = (originalPath, width) => getOutputPath(originalPath.replace(/(\d\d).jpg/, '$1' + `-${width}.jpg`));
+
+await fs.rm('./src/lib/generated', { recursive: true, force: true })
+
+////
+// img transformation
 
 const files = await glob('./gallery/**/*.jpg')
-const images = Object.keys(files)
+const folders = await glob('./gallery/*')
 
-images.forEach((image) => {
-    const path = image.split('gallery/')[1];
-    sharp(image)
-        .resize(150)
-        .toFile(`./src/lib/assests/${path}`)
+await Promise.all(
+    folders.map((folder) => fs.mkdir(getOutputPath(folder), { recursive: true }))
+)
+
+files.forEach((image) => {
+    SIZES.forEach((size) => {
+        sharp(image)
+            .resize(size)
+            .toFile(getImageDerivitePath(image, size))
+    })
 })

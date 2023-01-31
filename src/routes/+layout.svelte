0c58eaf698d536a325c9ff54f08fe2ccf9c03c4c
jycouet <jycouet@gmail.com>
2023-01-31 20:19:17 +0100

    :sparkles: NEW: 2023 Feb event

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
-	import { afterNavigate } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 
-	let title: string;
-
-	afterNavigate(({ to }) => {
-		const firstUrlSegment = to.url.pathname.split('/')[1] || 'Home';
-		title = firstUrlSegment.slice(0, 1).toUpperCase() + firstUrlSegment.slice(1);
-	});
+	const getTitle = () => {
+		const firstUrlSegment = $page.url.pathname.split('/')[1] || 'Home';
+		return firstUrlSegment.slice(0, 1).toUpperCase() + firstUrlSegment.slice(1);
+	};
 </script>
 
 <svelte:head>
-	<meta property="og:title" content="Svelte Society Austria - {title}" />
+	<meta property="og:title" content="Svelte Society Austria - {getTitle()}" />
 	<meta property="og:url" content={$page.url.href} />
 </svelte:head>
 
 <Header />
-<main><slot /></main>
+
+<main>
+	<slot />
+</main>
+
 <Footer />
 
 <style>
--- src/routes/meetups/meetups.ts
+++ src/routes/meetups/meetups.ts
@@ -1,48 +1,72 @@
 import type { TMeetup } from '$lib/components/TSvienna';
 export const meetups: TMeetup[] = [
-    {
-        dateISO: '2022-10-24T16:00:00.000Z',
-        name: '2022/10 Svienna - Svelte Society Vienna',
-        lumaLink: 'https://lu.ma/2022-10-Svienna',
-        by: 'svienna',
-        pictureCount: 14,
-        talks: [
-            {
-                name: 'The easy way - an introduction to Sveltekit',
-                githubAuthor: 'dreitzner'
-            },
-            {
-                name: 'Your Custom Component Library: Improving Workflows and DX in svelte',
-                githubAuthor: 'jjnp',
-            },
-            {
-                name: 'Test it with playwright, you must!',
-                githubAuthor: 'jycouet',
-            }
-        ]
-    },
-    {
-        dateISO: '2022-07-04T16:00:00.000Z',
-        name: '2022/07 Svienna - Svelte Society Vienna',
-        lumaLink: 'https://lu.ma/2022-07-Svienna',
-        by: 'svienna',
-        pictureCount: 10,
-        talks: [
-            {
-                name: 'From zero to package by the end of the talk',
-                githubAuthor: 'ivanhofer',
-                youtubeReplayLink: 'https://youtu.be/1chhhyCHVBw'
-            },
-            {
-                name: 'What if using GraphQL would be easy? Especially in SvelteKit!',
-                githubAuthor: 'jycouet',
-                youtubeReplayLink: 'https://youtu.be/Ggz5cvxDwFs'
-            },
-            {
-                name: 'Svackage - Svelte package inception with turborepo',
-                githubAuthor: 'dreitzner',
-                youtubeReplayLink: 'https://youtu.be/CjtlKDDJAf0'
-            }
-        ]
-    },
+	{
+		dateISO: '2023-02-20T16:00:00.000Z',
+		name: '2023/02 Svienna - Svelte Society Vienna',
+		lumaLink: 'https://lu.ma/2023-02-Svienna',
+		by: 'svienna',
+		pictureCount: 0,
+		talks: [
+			{
+				name: 'Your awesome title 🥳',
+				githubAuthor: ''
+			},
+			{
+				name: "An 'exceptionally' talk!",
+				githubAuthor: 'ivanhofer'
+			},
+			{
+				name: 'it will be great 🎉',
+				githubAuthor: ''
+			}
+		]
+	},
+	{
+		dateISO: '2022-10-24T16:00:00.000Z',
+		name: '2022/10 Svienna - Svelte Society Vienna',
+		lumaLink: 'https://lu.ma/2022-10-Svienna',
+		by: 'svienna',
+		pictureCount: 14,
+		talks: [
+			{
+				name: 'The easy way - an introduction to Sveltekit',
+				githubAuthor: 'dreitzner',
+				youtubeReplayLink: 'https://youtu.be/t-LKRrNedps'
+			},
+			{
+				name: 'Your Custom Component Library: Improving Workflows and DX in svelte',
+				githubAuthor: 'jjnp',
+				youtubeReplayLink: 'https://youtu.be/TQsJU_P1UBU'
+			},
+			{
+				name: 'Test it with playwright, you must!',
+				githubAuthor: 'jycouet',
+				youtubeReplayLink: 'https://youtu.be/svlsOKEF4QM'
+			}
+		]
+	},
+	{
+		dateISO: '2022-07-04T16:00:00.000Z',
+		name: '2022/07 Svienna - Svelte Society Vienna',
+		lumaLink: 'https://lu.ma/2022-07-Svienna',
+		by: 'svienna',
+		pictureCount: 10,
+		talks: [
+			{
+				name: 'From zero to package by the end of the talk',
+				githubAuthor: 'ivanhofer',
+				youtubeReplayLink: 'https://youtu.be/1chhhyCHVBw'
+			},
+			{
+				name: 'What if using GraphQL would be easy? Especially in SvelteKit!',
+				githubAuthor: 'jycouet',
+				youtubeReplayLink: 'https://youtu.be/Ggz5cvxDwFs'
+			},
+			{
+				name: 'Svackage - Svelte package inception with turborepo',
+				githubAuthor: 'dreitzner',
+				youtubeReplayLink: 'https://youtu.be/CjtlKDDJAf0'
+			}
+		]
+	}
 ];

ccea75674d5c757692cacb314cfe4a4324f36275
Domenik Reitzner <domenik@reitzner.at>
2023-04-11 13:51:59 +0200

    Add ISR to edge function 🍕

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,12 @@ import { deployUrl } from '$lib/util.server';
 import type { PageServerLoad } from './$types';
 import { meetups } from './meetups';
 
+export const config = {
+	isr: {
+		expiration: 3600,
+	}
+}
+
 export const load: PageServerLoad<{ meetups: TMeetup[] }> = () => {
 	return {
 		meetups,
--- src/routes/meetups/[...slug]/+page.server.ts
+++ src/routes/meetups/[...slug]/+page.server.ts
@@ -3,9 +3,14 @@ import { meetupToSlug } from '$lib/util';
 import { deployUrl } from '$lib/util.server';
 import { redirect } from '@sveltejs/kit';
 import { meetups } from '../../meetups';
-import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const config = {
+	isr: {
+		expiration: 3600,
+	}
+}
+
+export async function load({params}) {
     const item = meetups.find((m) => params.slug.startsWith(meetupToSlug(m.name)))
     if (!item) redirect(303, '/');
     return {

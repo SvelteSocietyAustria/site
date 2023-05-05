b566a85bb3611eff50df0e0edc37bde79bf10394
Domenik Reitzner <domenik@reitzner.at>
2023-05-05 11:09:46 +0200

    add guard for gallery 💂‍♂️

--- src/routes/meetups/[meetup]/gallery/[pictureId]/+page.server.ts
+++ src/routes/meetups/[meetup]/gallery/[pictureId]/+page.server.ts
@@ -1,4 +1,11 @@
-export const load = ({ params: { pictureId } }) => {
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ params: { pictureId }, url, parent }) => {
+	const meetupUrl = url.pathname.split('/gallery')[0]
+	const pictureInt = parseInt(pictureId)
+	if (Number.isNaN(pictureInt)) throw redirect(303, meetupUrl)
+	const { pictureCount } = await parent();
+	if (pictureInt < 0 || pictureInt > pictureCount) throw redirect(303, meetupUrl)
 	return {
 		pictureId,
 	};

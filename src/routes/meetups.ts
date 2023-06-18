a17d6cda47cea8a17d2292b8d9e830e0e93335df
jycouet <jycouet@gmail.com>
2023-06-18 21:51:02 +0200

    :sparkles: UPDATE: it's never too late

--- src/routes/meetups.ts
+++ src/routes/meetups.ts
@@ -1,6 +1,27 @@
 import type { TMeetup } from '$lib/components/TSvienna';
 export const meetups: TMeetup[] = [
 	{
+		dateISO: '2023-06-19T16:00:00.000Z',
+		name: '2023/06 Svienna - Svelte Society Vienna',
+		lumaLink: 'https://lu.ma/2023-06-svienna',
+		by: 'svienna',
+		pictureCount: 0,
+		talks: [
+			{
+				name: 'Super secret talk 😉',
+				githubAuthor: 'ermincelikovic',
+			},
+			{
+				name: 'Progressive enhancement with SvelteKit',
+				githubAuthor: 'dreitzner',
+			},
+			{
+				name: 'SvelteKit & Remult... fullstack apps in minutes!',
+				githubAuthor: 'jycouet',
+			},
+		],
+	},
+	{
 		dateISO: '2023-02-20T16:00:00.000Z',
 		name: '2023/02 Svienna - Svelte Society Vienna',
 		lumaLink: 'https://lu.ma/2023-02-Svienna',

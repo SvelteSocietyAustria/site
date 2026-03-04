d71cff386dab2f270cc1abe7532d5f682e182d79
Domenik Reitzner <domenik@reitzner.at>
2026-03-04 07:37:12 +0100

    Make the gallery URLs nicer 🦋

--- src/lib/util.server.ts
+++ src/lib/util.server.ts
@@ -1,3 +1,5 @@
-import { CF_PAGES_URL, VERCEL_URL } from '$env/static/private';
+import { VERCEL_URL, VERCEL_ENV } from '$env/static/private';
 
-export const deployUrl = CF_PAGES_URL || VERCEL_URL || 'austria.sveltesociety.dev';
+const prodURL = 'svelte.at';
+
+export const deployUrl = (VERCEL_ENV === 'production' ? prodURL : VERCEL_URL) || prodURL;
